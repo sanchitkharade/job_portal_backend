282511f6e203afb28549f255d412650461f88b23
Sanchit <sanchitkharade1120@gmail.com>
2025-03-31 19:04:16 +0530

    4

--- src/main/java/com/Jobportal/SecurityConfig.java
+++ src/main/java/com/Jobportal/SecurityConfig.java
@@ -52,7 +52,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000")); // Allow frontend origin
+        configuration.setAllowedOrigins(List.of("https://jobalchemy.onrender.com")); // Allow frontend origin
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true); // Allow cookies & credentials
